c6f15eb8ba478b3a288b6f39a3497ac6ac73cb9c
vivianlorenaortiz <973@holbertonschool.com>
2019-09-11 00:34:42 +0200

    print comb

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,9 @@ j = 0
 while (j < 10):
     i = j + 1
     while (i < 10):
-        if (i < 10 or j < 10):
+        if (j != 8):
             print("{:01d}{:01d}".format(j, i), end=", ")
+        else:
+            print("{:01d}{:01d}".format(j, i))
         i = i + 1
     j = j + 1
