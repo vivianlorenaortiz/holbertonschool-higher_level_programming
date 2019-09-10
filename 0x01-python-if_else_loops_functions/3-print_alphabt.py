49fe372e4ac8831438c173213e6a9135cd8af86b
vivianlorenaortiz <973@holbertonschool.com>
2019-09-10 21:36:35 +0200

    alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for alpha in range(ord('a'), ord('z') + 1):
-    if alpha in [101,113]:
-        continue
-    print("{:s}".format(chr(alpha)), end="")
+    if chr(alpha) is not "q" and chr(alpha) is not "e":
+        print("{:s}".format(chr(alpha)), end="")
