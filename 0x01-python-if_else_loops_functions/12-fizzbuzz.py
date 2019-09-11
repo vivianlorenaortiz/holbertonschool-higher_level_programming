d9d9a8bcd4711084b1177d06d19cd57e2b9536d6
vivianlorenaortiz <973@holbertonschool.com>
2019-09-11 04:46:42 +0200

    fix fizz buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for x in range(1, 101):
         if x % 3 == 0 and x % 5 == 0:
-           print("FizzBuzz ", end="")
+            print("FizzBuzz ", end="")
         elif x % 3 == 0:
             print("Fizz ", end="")
         elif x % 5 == 0:
