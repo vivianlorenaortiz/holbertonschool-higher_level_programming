55fab52e103caf80a163557007f3e9237f3e3029
vivianlorenaortiz <973@holbertonschool.com>
2019-09-13 00:04:30 +0200

    infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     from sys import argv
     l = len(argv)
     addition = 0
-    for i in range(1, 1):
+    for i in range(1, l):
         addition += int(argv[i])
     print("{:d}".format(addition))
