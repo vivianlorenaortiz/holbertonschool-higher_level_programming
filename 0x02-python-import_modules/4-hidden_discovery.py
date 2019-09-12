51a890c4fa9a81b83fef635d7a8997d87467d428
vivianlorenaortiz <973@holbertonschool.com>
2019-09-13 01:07:10 +0200

    hidden

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import hidden_4
+    names = dir(hidden_4)
+    for i in range(len(names)):
+        if names[i][0:2] != "__":
+            print(names[i])
