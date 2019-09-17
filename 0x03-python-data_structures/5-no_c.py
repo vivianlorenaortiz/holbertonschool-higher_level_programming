69d9955993b1421dc2ff271167c45da7b0e1b6b4
vivianlorenaortiz <973@holbertonschool.com>
2019-09-18 01:42:43 +0200

    function that removes

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string  = ""
+
+    for i in my_string:
+        if i is not 'c' and i is not 'C':
+            new_string += i
+
+            return new_string
