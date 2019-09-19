8972c4ec72a16678cc6ddb859fa1578b11481979
vivianlorenaortiz <973@holbertonschool.com>
2019-09-19 22:39:56 +0200

    fix space

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,6 +4,6 @@ def search_replace(my_list, search, replace):
     for i in my_list:
         if i == search:
             new_list.append(replace)
-        else :
+        else:
             new_list.append(i)
     return new_list
