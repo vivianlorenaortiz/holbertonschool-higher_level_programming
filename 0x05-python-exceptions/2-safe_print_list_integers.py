6d860157f92a201fab93ff79edd380714b719812
vivianlorenaortiz <973@holbertonschool.com>
2019-09-30 22:47:53 +0200

    review os exercise

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
@@ -8,5 +9,5 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except (ValueError, TypeError):
             continue
-    print ("")
+    print("")
     return count
