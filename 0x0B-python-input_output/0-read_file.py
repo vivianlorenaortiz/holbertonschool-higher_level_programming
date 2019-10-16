4d83e87eea7847d7075d5a4e2a4dfce79c43437e
vivianlorenaortiz <973@holbertonschool.com>
2019-10-17 01:12:13 +0200

    read line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,5 @@
 
 def read_file(filename=""):
     """ reads a text file (UTF8) and prints it to stdout """
-    with open(filename, encoding='utf-8') as f:
-              for line in f:
-                  print(line, end="")
+    with open(filename, encoding='utf-8') as myFile:
+                  print(myFile.read(), end="")
