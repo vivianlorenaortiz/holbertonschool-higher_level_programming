8003554d57836f94a04c116c5b098d9959c39f2f
vivianlorenaortiz <973@holbertonschool.com>
2019-10-17 01:03:39 +0200

    json strng

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" function that returns an object (Python data structure) """
+import json
+
+
+def from_json_string(my_str):
+    """ represented by a JSON string: """
+    return json.loads(my_str)
