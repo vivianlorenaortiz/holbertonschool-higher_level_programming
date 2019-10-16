1dcef1e95a9376890d92db3dec0176044730b4c0
vivianlorenaortiz <973@holbertonschool.com>
2019-10-16 19:30:55 +0200

    review pep8

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def inherits_from(obj, a_class):
-    """
-   function python   """
+    """ returns True if the object in an instance of a class
+    that inherited (directly or indirectly) from a specified class """
     if issubclass(type(obj), a_class) and type(obj) is a_class:
         return False
     else:
