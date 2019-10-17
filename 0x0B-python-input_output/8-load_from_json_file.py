fb2ea063a15d4c8f6484402c86eb560d41db9369
vivianlorenaortiz <973@holbertonschool.com>
2019-10-17 05:00:27 +0200

    load json file

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import json
+
+
+def load_from_json_file(filename):
+    """ creates an obj from a json file """
+    with open(filename, encoding='utf-8') as f:
+        return json.load(f)
