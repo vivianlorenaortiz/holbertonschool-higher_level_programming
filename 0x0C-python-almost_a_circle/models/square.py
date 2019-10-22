0d22c67e8c857f3d5d76d88cf69caddb33d360c1
vivianlorenaortiz <973@holbertonschool.com>
2019-10-22 23:37:33 +0200

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,28 +16,6 @@ class Square(Rectangle):
                                                  self.y,
                                                  self.width)
 
-    def to_dictionary(self):
-        """
-        to dict
-        """
-        return {
-            'id': self.id,
-            'size': self.size,
-            'x': self.x,
-            'y': self.y
-        }
-
-    def update(self, *args, **kwargs):
-        """
-        update square
-        """
-        tmpArr = ["id", "size", "x", "y"]
-        if kwargs is not None:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-        for x, arg in enumerate(args):
-            setattr(self, tmpArr[x], arg)
-
     @property
     def size(self):
         return self.width
@@ -46,3 +24,38 @@ class Square(Rectangle):
     def size(self, val):
         self.width = val
         self.height = val
+
+    def update(self, *args, **kwargs):
+        """
+        update square
+        """
+        if len(args):
+            for i, a in enumerate(args):
+                if i == 0:
+                    self.id = a
+                elif i == 1:
+                    self.size = a
+                elif i == 2:
+                    self.x = a
+                elif i == 3:
+                    self.y = a
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """
+        to dict
+        """
+        n = {}
+        'id' = self.id,
+        'size' = self.size,
+        'x' = self.x,
+        'y' = self.y
+        return n
