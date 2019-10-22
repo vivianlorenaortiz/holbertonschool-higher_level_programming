d1f975e9756824e2cdad83e6553f7bd8ad74f6b9
vivianlorenaortiz <973@holbertonschool.com>
2019-10-22 23:17:26 +0200

    review coding pep8

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,8 +11,10 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format \
-        (self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(self.id,
+                                                 self.x,
+                                                 self.y,
+                                                 self.width)
 
     def to_dictionary(self):
         """
@@ -39,6 +41,7 @@ class Square(Rectangle):
     @property
     def size(self):
         return self.width
+
     @size.setter
     def size(self, val):
         self.width = val
