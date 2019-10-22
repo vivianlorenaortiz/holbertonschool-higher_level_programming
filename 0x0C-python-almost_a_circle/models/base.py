27f9284224cd09213cc04bdefb25f8e12c59c9e0
vivianlorenaortiz <973@holbertonschool.com>
2019-10-23 01:54:09 +0200

    nf

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,28 +23,29 @@ class Base:
         """ JSON string """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if list_objs is not None and len(list_objs) > 0:
+            empty_list = []
+            for i in list_objs:
+                my_dict = i.to_dictionary()
+                empty_list.append(my_dict)
+            with open(cls.__name__ + ".json", mode='w', encoding="utf-8") as f:
+                    f.write(cls.to_json_string(empty_list))
+        else:
+            with open(cls.__name__ + ".json", mode='w', encoding="utf-8") as f:
+                    f.write(cls.to_json_string(list_objs))
 
     @staticmethod
     def from_json_string(json_string):
         """ list of the JSON string"""
-        if json_string is None or len(json_string) == 0:
-            return []
+        empty_list = []
+        if json_string is None:
+            return empty_list
         return json.loads(json_string)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """writes the JSON string """
-        temp = []
-        if list_objs is None or len(list_objs) is 0:
-            pass
-        else:
-            for i in list_objs:
-                temp.append(cls.to_dictionary(i))
-            x = cls.to_json_string(temp)
-            with open("{}.json".format(cls.__name__),
-                      mode='w', encoding='utf-8') as f:
-                f.write(x)
 
     @classmethod
     def create(cls, **dictionary):
