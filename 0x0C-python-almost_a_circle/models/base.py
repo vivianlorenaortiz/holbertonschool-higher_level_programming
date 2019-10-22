8358b21491af68734220084857bb5274750c773c
vivianlorenaortiz <973@holbertonschool.com>
2019-10-23 00:49:24 +0200

    add base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,7 +11,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize the base class"""
+        """ base class """
         if id is None:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
@@ -20,14 +20,21 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """returns the JSON string representation of a list of dictionaries"""
+        """ JSON string """
         if list_dictionaries is None:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
 
+    @staticmethod
+    def from_json_string(json_string):
+        """ list of the JSON string"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
-        """writes the JSON string representation of list_objs to a file"""
+        """writes the JSON string """
         filename = cls.__name__ + ".json"
         lo = []
         if list_objs is not None:
@@ -36,16 +43,9 @@ class Base:
         with open(filename, 'w') as f:
             f.write(cls.to_json_string(lo))
 
-    @staticmethod
-    def from_json_string(json_string):
-        """returns the list of the JSON string representation json_string"""
-        if json_string is None or len(json_string) == 0:
-            return []
-        return json.loads(json_string)
-
     @classmethod
     def create(cls, **dictionary):
-        """returns an instance with all attributes already set"""
+        """instance with all attributes"""
         if cls.__name__ is "Rectangle":
             dummy = cls(1, 1)
         elif cls.__name__ is "Square":
