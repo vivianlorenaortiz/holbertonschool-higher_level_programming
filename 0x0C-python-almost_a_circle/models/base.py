02dec1e1d16bcfc5089eae09292bd5292bbedf0c
vivianlorenaortiz <973@holbertonschool.com>
2019-10-23 01:17:29 +0200

    fix pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,8 +21,8 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ JSON string """
-        if list_dictionaries is None:
-            list_dictionaries = []
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @staticmethod
