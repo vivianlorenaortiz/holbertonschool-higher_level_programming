771e7f2338fa463138ca4ae66b09964ad7e03796
vivianlorenaortiz <973@holbertonschool.com>
2019-10-23 00:18:56 +0200

    adding file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,51 +1,57 @@
 #!/usr/bin/python3
-"""base.py"""
+"""
+This module contains the "Base" class
+"""
+
 import json
 
 
 class Base:
-    """ Base Class """
-
-    _nb_objects = 0
+    """A base class"""
+    __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialize the base class"""
         if id is None:
-            Base._nb_objects += 1
-            self.id = Base._nb_objects
-
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
         else:
             self.id = id
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """returns the JSON string representation of a list of dictionaries"""
         if list_dictionaries is None:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
 
     @staticmethod
     def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
         filename = cls.__name__ + ".json"
         lo = []
         if list_objs is not None:
-            for x in list_objs:
-                lo.append(cls.to_dictionary(x))
-        with open(filename, "w+") as f:
+            for i in list_objs:
+                lo.append(cls.to_dictionary(i))
+        with open(filename, 'w') as f:
             f.write(cls.to_json_string(lo))
 
     @classmethod
     def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
         if cls.__name__ is "Rectangle":
-                    tmp = cls(1, 1)
+            dummy = cls(1, 1)
         elif cls.__name__ is "Square":
-            tmp = cls(1)
-        tmp.update(**dictionary)
-        return tmp
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
 
     @classmethod
     def load_from_file(cls):
