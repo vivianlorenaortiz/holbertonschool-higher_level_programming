b2759a0579c1ca238be5cddaa6167fa326531f24
vivianlorenaortiz <973@holbertonschool.com>
2019-10-23 05:12:41 +0200

    addind file test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -12,29 +12,44 @@ class Test_BaseClass(unittest.TestCase):
     unittest base test class.
     """
 
-    def test_basic_case(self):
-        self.assertEqual(max_integer([1, 2, 3, 6]), 6)
-        self.assertEqual(max_integer([5, 6, 1, 8]), 8)
-        self.assertEqual(max_integer([21, 23, 32, 71]), 71)
+     def test_standard(self):
+        b1 = Base(12); b2 = Base(-45)
+        b3 = Base(); b4 = Base()
+        self.assertEqual(b1.id, 12); self.assertEqual(b2.id, -45)
+        self.assertEqual(type(b3.id), int); self.assertEqual(type(b4.id), int)
 
-    def text_negative_case(self):
-        self.assertEqual(max_integer([-1, -2, -3, -6]), -6)
-        self.assertEqual(max_integer([-5, -6, -1, -8, -2]), -1)
-        self.assertEqual(max_integer([21, 23, 32, 71]), -71)
+    def test_not_int_id(self):
+        b1 = Base([2, 3, 4])
+        self.assertEqual(b1.id, [2, 3, 4])
+        b2 = Base("Wasabi")
+        self.assertEqual(b2.id, "Wasabi")
 
-    def test_basic_empty(self):
-        self.assertEqual(max_integer([]), None)
+    def test_conflicting_id(self):
+        b1 = Base()
+        b2 = Base(1)
+        self.assertEqual(type(b1.id), int)
+        self.assertEqual(b2.id, 1)
 
-    def test_basic_float(self):
-        self.assertEqual(max_integer([0.65, 2.4, 3.0]), 3.0)
-        self.assertEqual(max_integer([12.9, 8.1, 8.0]), 12.9)
-        self.assertEqual(max_integer([11.2, 5.6, 20.1, 15.3]), 20.1)
+    def test_reg_dict_to_string(self):
+        b = Base()
+        tmpDict = {'x': 1, 'y': 8, 'id': 1, 'height': 3, 'width': 9}
+        self.assertEqual(type(b.to_json_string(tmpDict)), str)
 
-    def test_basic_oneelement(self):
-        self.assertEqual(max_integer([1]), 1)
+    def test_emptyDict(self):
+        b = Base()
+        self.assertEqual(b.to_json_string(None), "[]")
 
-    def test_basic_string(self):
-        self.assertEqual(max_integer(""), None)
+    def test_from_toDict_to_str(self):
+        r = Rectangle(10, 15, 13, 37, id=55)
+        self.assertEqual(type(r.to_dictionary()), dict)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_fromJsonStr(self):
+        b = Base()
+        tmpDictStr = "{\"x\": 1, \"width\": 8, \"id\": 4, \"height\": 6, \"y\": 8}"
+        self.assertEqual(type(b.from_json_string(tmpDictStr)), dict)
+        tmpDictStr = "[{\"x\": 1, \"width\": 8, \"id\": 4, \"height\": 6, \"y\": 8}]"
+        self.assertEqual(type(b.from_json_string(tmpDictStr)), list)
+
+    def test_fromJsonStr_wit_empty_str(self):
+        b = Base()
+        self.assertEqual(type(b.from_json_string(None)), list)
