302b2a1742a0d12b7f5f4d40e9cfc476badd27b7
vivianlorenaortiz <973@holbertonschool.com>
2019-10-23 04:43:31 +0200

    adding file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -12,12 +12,11 @@ class Base:
 
     def __init__(self, id=None):
         """ base class """
-        if id is None:
-            Base.__nb_objects += 1
+        if id is not None:
             self.id = id
         else:
-            Base.__nb_object += 1
-            self.id = Base.__nb_object
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
