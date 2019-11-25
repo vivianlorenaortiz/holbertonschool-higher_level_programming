8fa62b7565da9e2ab33d9d3a4907c25f20e75b8d
vivianlorenaortiz <973@holbertonschool.com>
2019-11-25 23:48:02 +0100

    scrip that display the number of records

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- script that displays the number of records with id = 89 in the table.
+-- The database name will be passed as an argument of the mysql command
+
+SELECT COUNT(*) FROM first_table WHERE id=89;
