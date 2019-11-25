7450ca8452734481a9c55a4575dcb4e1b5ea1bd8
vivianlorenaortiz <973@holbertonschool.com>
2019-11-25 23:57:23 +0100

    full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- script that creates a table second_table in the database hbtn_0c_0
+-- The database name will be passed as an argument to the mysql command
+-- If the table second_table already exists, your script should not fail
+-- You are not allowed to use the SELECT and SHOW statements.
+
+CREATE TABLE IF NOT EXISTS second_table (id INT,name VARCHAR(256),score INT);
+INSERT INTO second_table (id, name, score)
+VALUES (1, "John", 10),(2, "Alex", 3),(3, "Bob", 14),(4, "George", 8);
