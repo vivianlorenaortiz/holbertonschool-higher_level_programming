5312afd84bc26dec01d8507f0a9c13dfed9c025e
vivianlorenaortiz <973@holbertonschool.com>
2019-11-25 23:36:22 +0100

    Full description

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- script that prints the full description of the table first_table
+-- The database name will be passed as an argument of the mysql command
+-- You are not allowed to use the DESCRIBE or EXPLAIN statements
+
+SHOW CREATE TABLE first_table;
