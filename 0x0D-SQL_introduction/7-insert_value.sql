0cbe2a8746eaa0899c87d01b16e4f00a43e1f6da
vivianlorenaortiz <973@holbertonschool.com>
2019-11-25 23:45:15 +0100

    insert value

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- script that inserts a new row in the table first_table.
+-- The database name will be passed as an argument of the mysql command.
+
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Holberton School');
