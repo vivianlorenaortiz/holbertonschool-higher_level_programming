3fdaa25f44f1ce284bfd05d61a003cc221369260
vivianlorenaortiz <973@holbertonschool.com>
2019-11-27 01:15:59 +0100

    scrip creates the table force name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,2 @@
 --script that creates the table force_name on your MySQL server.
-
-CREATE TABLE IF NOT EXISTS force_name(id, name VARCHAR(256)NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name(id, name VARCHAR(256) NOT NULL);
