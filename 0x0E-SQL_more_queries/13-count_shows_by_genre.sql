003d5c5c3f6f1f64474e9673a330f1314930d7ec
vivianlorenaortiz <973@holbertonschool.com>
2019-11-27 04:01:27 +0100

    lists all genres

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- script that lists all shows contained in hbtn_0d_tvshows.
 
-SELECT tv_genres.name AS genre, COUNT(tv_show_genre.genre_id) AS number_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
 FROM tv_show_genres
 INNER JOIN  tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
