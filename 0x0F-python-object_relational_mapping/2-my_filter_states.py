e9066fde747d909c53c49f48a05a35ee164570c6
vivianlorenaortiz <973@holbertonschool.com>
2019-12-21 00:19:28 +0100

    filter file

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,20 +5,18 @@ script that lists all states with a name starting.
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db = sys.argv[3]
-    name = sys.argv[4]
-    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_user,
-                         passwd=mysql_passwd, db=mysql_db)
+
+    db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id".
-                format(mysql_name))
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        if row[1] == mysql_name:
-            print(row)
+    query = "SELECT * FROM states where name\
+            LIKE BINARY '{}'".format(sys.argv[4])
+    cur.execute(query)
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
     cur.close()
     db.close()
