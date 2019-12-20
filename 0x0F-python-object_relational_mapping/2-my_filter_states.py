30d99c09bd65f1da3b97cc95aef7325339e7a235
vivianlorenaortiz <973@holbertonschool.com>
2019-12-20 18:39:58 +0100

    filter stat

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+script that lists all states with a name starting.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    name = sys.argv[3]
+
+    db = MySQLdb.connect("localhost", username, password, name)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id".
+                format(mysql_name))
+    query_rows = cur.fetchall()
+    for row in rows:
+        if row[1] == mysql_name:
+            print(row)
+    cur.close()
+    db.close()
