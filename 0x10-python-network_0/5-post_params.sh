d98b212b966c1ce1937f5f429a92257c08550979
vivianlorenaortiz <973@holbertonschool.com>
2020-01-07 23:06:46 +0100

    post params

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL, sends a POST request to the passed URL.
-curl $1 -sX POST -d "email:hr@holbertonschool.com&subject:I will always be here for PLD"
+curl -s $1 -X POST -d "email:hr@holbertonschool.com&subject:I will always be here for PLD"
