a4294ec7e016e7bc87527c6ae9fb78389ae0d578
vivianlorenaortiz <973@holbertonschool.com>
2020-01-07 22:47:28 +0100

    methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL and displays all HTTP methods.
-curl -is $1 | grep "Allow": | sed 's/Allow://g'
+curl -sI $1 | grep "Allow": | sed 's/Allow: //g'
