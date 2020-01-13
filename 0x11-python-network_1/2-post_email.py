51d460f0eb5a62419ecb44f58445b8c782bd0cd7
vivianlorenaortiz <973@holbertonschool.com>
2020-01-13 23:24:24 +0100

    post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+post request
+"""
+import urllib.parse
+import urllib.request
+import sys
+
+if __name__== "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    values = {'email': email}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+    print(the_page.decode(encoding="utf-8"))
