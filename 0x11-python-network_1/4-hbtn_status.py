5df0d5ab85bf1531dad90bc0b0d0e7728d60f9fe
vivianlorenaortiz <973@holbertonschool.com>
2020-01-14 01:17:02 +0100

    hbtb status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ Write a Python script that fetches https://intranet.hbtn.io/status.
 
 import requests
 
-if __name__ "__main__":
+if __name__ == "__main__":
     r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
