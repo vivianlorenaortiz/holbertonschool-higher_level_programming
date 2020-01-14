25d404f3c87e14d9735871c8261c5883dfd0b395
vivianlorenaortiz <973@holbertonschool.com>
2020-01-14 03:42:05 +0100

    json api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL and an email address, sends a POST request.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    try:
+        payload = {'q': sys.argv[1]}
+        r = requests.post('http://0.0.0.0:5000/search_user', payload)
+    except:
+        r = requests.post('http://0.0.0.0:5000/search_user')
+    try:
+        print("[{}] {}".format(r.json()["id"], r.json()["name"]))
+    except:
+        if not r.json():
+            print("No result")
