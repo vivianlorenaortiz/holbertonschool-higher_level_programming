8f61853f449d48b0efd69072fc18ebd9a4e8a606
vivianlorenaortiz <973@holbertonschool.com>
2020-01-14 03:56:44 +0100

    json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,12 +2,9 @@
 """
 script that takes in a URL and an email address, sends a POST request.
 """
-
 import requests
 import sys
-
 if __name__ == "__main__":
-
     try:
         payload = {'q': sys.argv[1]}
         r = requests.post('http://0.0.0.0:5000/search_user', payload)
